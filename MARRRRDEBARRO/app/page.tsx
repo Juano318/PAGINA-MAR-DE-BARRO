a9bf8f3416eaa1bf01479c1588949335f976da8b
juano <juanocvte@gmail.com>
2025-04-28 17:37:38 -0300

    sin xd

--- MARRRRDEBARRO/app/page.tsx
+++ MARRRRDEBARRO/app/page.tsx
@@ -31,7 +31,7 @@ export default function Home() {
               size="lg"
               className="bg-blue-600 hover:bg-blue-700 text-white"
             >
-              <Link href="/productos">Ver Productos XD</Link>
+              <Link href="/productos">Ver Productos</Link>
             </Button>
             <Button
               asChild
